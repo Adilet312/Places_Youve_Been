bee69a0db0d449012b9782fb44ae5591924840cf
Adilet Momunaliev, Sharon Lee and Vera Weikel <adilet+sharon+vera@>
2020-01-08 15:22:11 -0800

    Init files for MVC places visited.

---
+++ PlacesYouveBeen/Controllers/PlacesController.cs
@@ -0,0 +1,55 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PlacesYouveBeen.Models;
+using System.Collections.Generic;
+
+namespace PlacesYouveBeen.Controllers
+{
+    public class PlacesController : Controller
+    {
+        //ListPlace list = new ListPlace();
+
+        [HttpGet("/places")]
+        public ActionResult Index()
+        {
+            List<Place> placesList = Place.getAllPlaces();
+            return View(placesList);
+            
+            //return View(list.toStirng());
+        }
+        [HttpGet("/places/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/places")]
+        public ActionResult Index(string cityname, string picturelink, string journal, int lengthofstay)
+        {
+            Place myplaces = new Place(cityname, 
+             picturelink, journal, lengthofstay);
+            //list.addPlace(places);
+            // Console.WriteLine("This line.");
+            // Console.WriteLine("City name: "+cityname + ", Picture link: " + picturelink + ", Journal: " +journal +
+            // ", Length of the stay: " + lengthofstay);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("/places/delete")]
+        public ActionResult Delete()
+        {
+            Place.clearAllPlaces();
+            return View();
+        }
+
+        [HttpGet("/places/show")]
+        public ActionResult Show(int findId)
+        {
+            Place findPlace = Place.Find(findId);
+            return View(findPlace);
+        }
+
+       
+    }
+}
---
+++ PlacesYouveBeen/Models/Places.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic; 
+
+namespace PlacesYouveBeen.Models
+{
+    public class Place
+    {
+        private string cityname;
+        private string picturelink;
+        private string journal;
+        private int lengthofstay;
+        private string[] companions;
+        private int id;
+        private static List<Place> placeslist = new List<Place>{};
+
+        public Place(string cityname, string picturelink, string journal, int lengthofstay)
+        {
+            this.cityname = cityname;
+            this.picturelink = picturelink;
+            this.journal = journal;
+            this.lengthofstay = lengthofstay;
+            this.companions = null;
+            placeslist.Add(this);
+            this.id = placeslist.Count;
+        }
+        public Place(string cityname, string picturelink, string journal, int lengthofstay, string[] companions)
+        {
+            this.cityname = cityname;
+            this.picturelink = picturelink;
+            this.journal = journal;
+            this.lengthofstay = lengthofstay;
+            this.companions = companions;
+            placeslist.Add(this);
+            this.id = placeslist.Count;
+        }
+
+        public string getCityName()
+        {
+            return this.cityname;
+        }
+        public void setCityName(string newname)
+        {
+            this.cityname = newname;
+        }
+        public string getPictureLink()
+        {
+            return this.picturelink;
+        }
+        public void setPictureLink(string newpicture)
+        {
+            this.picturelink = newpicture;
+        }
+
+        public string getJournal()
+        {
+            return this.journal;
+        }
+        public void setJournal(string newjournal)
+        {
+            this.journal = newjournal;
+        }
+        public int getLengthOfStay()
+        {
+            return this.lengthofstay;
+        }
+        public void setLengthOfStay(int newlengthofstay)
+        {
+            this.lengthofstay = newlengthofstay;
+        }
+        public string[] getCompanions()
+        {
+            return this.companions;
+        }
+        public void setCompanions(string[] newcompanions)
+        {
+            this.companions = newcompanions;
+        }
+         public int getId()
+        {
+            return this.id;
+        }
+
+         public static List<Place> getAllPlaces()
+        {
+            return placeslist;
+        }
+        public static void clearAllPlaces()
+        {
+            placeslist.Clear();
+        }
+        public static Place Find(int searchId)
+        {
+            return placeslist[searchId-1];
+        }
+        public string toString()
+        {
+            string result ="City name: "+this.cityname + ", Picture link: " + this.picturelink + ", Journal: " +this.journal +
+            ", Length of the stay: " + this.lengthofstay  + ", Id: " + this.id;
+            return result;
+        }
+
+    }
+}
